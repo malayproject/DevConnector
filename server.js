0ae503c6f472f2cba70bdf7fa677fc7cc83e484b
malayproject <malay.s1006@gmail.com>
2020-12-21 15:44:15 +0530

    ready for deployment

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 
+//connect database
 connectDB();
 
 // init middleware
 app.use(express.json({ extended: false }));
 
-var port = process.env.port || 5050;
-app.listen(port, () => console.log(`listening at port ${port}`));
+//app.get('/', (req, res) => res.send('homepage showing.'));
 
-app.get('/', (req, res) => res.send('homepage showing.'));
+//define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production')   {
+
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'inex.html'));
+    })
+}
+
+var port = process.env.port || 5050;
+app.listen(port, () => console.log(`listening at port ${port}`));
